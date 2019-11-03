eece30a67955ef9b53630c0bf9206fa809ee69a9
vaughn <vaughncato@gmail.com>
2019-11-03 09:12:19 -0500

    Made scene start unexpanded.

--- qttreewidgetitem.cpp
+++ qttreewidgetitem.cpp
@@ -16,7 +16,7 @@ QTreeWidgetItem& insertChildItem(QTreeWidgetItem &parent_item,int index)
   QTreeWidgetItem *item_ptr = new QTreeWidgetItem;
   parent_item.insertChild(index,item_ptr);
   QTreeWidgetItem &item = *item_ptr;
-  item.setExpanded(true);
+  item.setExpanded(false);
   return item;
 }
 
