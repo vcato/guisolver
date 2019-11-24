8b3fe7125c4c63e2d569cadfdf856a386b5a8fd4
vaughn <vaughncato@gmail.com>
2019-11-24 11:32:52 -0500

    Refactoring.

--- main.cpp
+++ main.cpp
@@ -11,6 +11,20 @@
 using std::cerr;
 
 
+static void createGraphicsWindow(QBoxLayout &layout, OSGScene &scene)
+{
+  GraphicsWindowPtr graphics_window_ptr =
+    scene.createGraphicsWindow(ViewType::free);
+
+  graphics_window_ptr->getGLWidget()->setDefaultSize(QSize(640,480));
+    // The default size here doesn't seem to really work.  It defaults to
+    // a size that is much smaller than 640x480, but if we don't set the default
+    // size at all, then it ends up collapsing to 0 size.
+
+  layout.addWidget(graphics_window_ptr->getGLWidget());
+}
+
+
 int main(int argc,char** argv)
 {
   QApplication app(argc,argv);
@@ -19,19 +33,11 @@ int main(int argc,char** argv)
   main_window.show();
   OSGScene scene;
 
-  GraphicsWindowPtr graphics_window_ptr =
-    scene.createGraphicsWindow(ViewType::free);
-
-  // The default size here doesn't seem to really work.  It defaults to
-  // a size that is much smaller than 640x480, but if we don't set the default
-  // size at all, then it ends up collapsing to 0 size.
-  graphics_window_ptr->getGLWidget()->setDefaultSize(QSize(640,480));
-
   QWidget central_widget;
   main_window.setCentralWidget(&central_widget);
   QBoxLayout &layout = createLayout<QHBoxLayout>(central_widget);
   QtTreeWidget &tree_widget = createWidget<QtTreeWidget>(layout);
-  layout.addWidget(graphics_window_ptr->getGLWidget());
-  MainWindowController controller(scene,tree_widget);
+  createGraphicsWindow(layout, scene);
+  MainWindowController controller(scene, tree_widget);
   app.exec();
 }
